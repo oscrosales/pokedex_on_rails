64684bc2ff4cc5b87c560218570b0b11c68f3d59
oscrosales <orosales2@academic.rrc.ca>
2025-03-03 11:31:26 -0600

    Seeded Types table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
 require "poke-api-v2"
 
-types = PokeApi.get(:type).results
+# types = PokeApi.get(:type).results
 
-types.each do |x|
-  type = PokeApi.get(type: x.name)
-  unless type.generation.name != "generation-i"
-    puts type.name
-  end
-end
+# types.each do |x|
+#   type = PokeApi.get(type: x.name)
+#   unless type.generation.name != "generation-i"
+#     Type.create(
+#       name: type.name.capitalize
+#     )
+#     puts "Created #{type.name.capitalize} type"
+#   end
+# end
+
+abilities = PokeApi.get(:ability)
+
+puts abilities.inspect
