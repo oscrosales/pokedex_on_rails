70ac681cc9f2185b325e78e7aaad2a2b24fbfad7
oscrosales <orosales2@academic.rrc.ca>
2025-03-04 14:15:43 -0600

    Added the associations to the Pokemon and Type models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,7 @@
 class Pokemon < ApplicationRecord
   has_one :species
+  has_many :pokemon_types
+  has_many :types, through: :pokemon_types
 
   validates :name, :height, :weight, :hp, :attack, :defense, :spatk, :spdef, :speed, :sprite, presence: true
   validates :height, :weight, :hp, :attack, :defense, :spatk, :spdef, :speed, numericality: { only_integer: true }
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,7 @@
 class Type < ApplicationRecord
+  has_many :pokemon_types
+  has_many :pokemons, through: :pokemon_types
+
   validates :name, presence: true
   validates :name, uniqueness: true
 end
