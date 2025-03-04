2696f186b13a6a41f20f83d95fc3f3074f71973e
oscrosales <orosales2@academic.rrc.ca>
2025-03-04 15:43:16 -0600

    Created validation for PokemonType model to ensure pokemon cannot have more than two types.

--- app/models/pokemon_type.rb
+++ app/models/pokemon_type.rb
@@ -1,4 +1,14 @@
 class PokemonType < ApplicationRecord
   belongs_to :pokemon
   belongs_to :type
+
+  validates :two_types
+
+  private
+
+  def two_types
+    if pokemon.pokemon_types.count >= 2
+      errors.add(:pokemon, "can't have more than 2 types")
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,101 +1,101 @@
 require "poke-api-v2"
 
-# pokemons = PokeApi.get(pokemon: { limit: 151 }).results
+pokemons = PokeApi.get(pokemon: { limit: 151 }).results
 
-# pokemons.each do |x|
-#   pokemon = PokeApi.get(pokemon: x.name)
-#   hp = 0
-#   attack = 0
-#   defense = 0
-#   spatk = 0
-#   spdef = 0
-#   speed = 0
+pokemons.each do |x|
+  pokemon = PokeApi.get(pokemon: x.name)
+  hp = 0
+  attack = 0
+  defense = 0
+  spatk = 0
+  spdef = 0
+  speed = 0
 
-#   pokemon.stats.each do |stat|
-#     if stat.stat.name === "hp"
-#       hp = stat.base_stat
-#     end
+  pokemon.stats.each do |stat|
+    if stat.stat.name === "hp"
+      hp = stat.base_stat
+    end
 
-#     if stat.stat.name === "attack"
-#       attack = stat.base_stat
-#     end
+    if stat.stat.name === "attack"
+      attack = stat.base_stat
+    end
 
-#     if stat.stat.name === "defense"
-#       defense = stat.base_stat
-#     end
+    if stat.stat.name === "defense"
+      defense = stat.base_stat
+    end
 
-#     if stat.stat.name === "special-attack"
-#       spatk = stat.base_stat
-#     end
+    if stat.stat.name === "special-attack"
+      spatk = stat.base_stat
+    end
 
-#     if stat.stat.name === "special-defense"
-#       spdef = stat.base_stat
-#     end
+    if stat.stat.name === "special-defense"
+      spdef = stat.base_stat
+    end
 
-#     if stat.stat.name === "speed"
-#       speed = stat.base_stat
-#     end
-#   end
+    if stat.stat.name === "speed"
+      speed = stat.base_stat
+    end
+  end
 
-#   Pokemon.create(
-#     name: pokemon.name.capitalize,
-#     height: pokemon.height,
-#     weight: pokemon.weight,
-#     hp: hp,
-#     attack: attack,
-#     defense: defense,
-#     spatk: spatk,
-#     spdef: spdef,
-#     speed: speed
-#     sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/#{pokemon.id}.svg"
-#   )
+  Pokemon.create(
+    name: pokemon.name.capitalize,
+    height: pokemon.height,
+    weight: pokemon.weight,
+    hp: hp,
+    attack: attack,
+    defense: defense,
+    spatk: spatk,
+    spdef: spdef,
+    speed: speed
+    sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/#{pokemon.id}.svg"
+  )
 
-#   pokemon.types.each do |type|
-#     t = Type.find_or_create_by(name: type.type.name.capitalize)
+  pokemon.types.each do |type|
+    t = Type.find_or_create_by(name: type.type.name.capitalize)
 
-#     if t.persisted?
-#       PokemonType.create(pokemon: pokemon, type: t)
-#     end
-#   end
+    if t.persisted?
+      PokemonType.create(pokemon: pokemon, type: t)
+    end
+  end
 
-#   pokemon.abilities.each do |x|
-#     ability = PokeApi.get(ability: x.ability.name)
+  pokemon.abilities.each do |x|
+    ability = PokeApi.get(ability: x.ability.name)
 
-#     effect = ""
+    effect = ""
 
-#     ability.flavor_text_entries.each do |entry|
-#       effect = entry.flavor_text.squish unless entry.language.name != "en" || entry.version_group.name != "platinum"
-#     end
+    ability.flavor_text_entries.each do |entry|
+      effect = entry.flavor_text.squish unless entry.language.name != "en" || entry.version_group.name != "platinum"
+    end
 
-#     a = Ability.find_or_create_by(
-#       name: ability.name.capitalize,
-#       effect: effect
-#     )
+    a = Ability.find_or_create_by(
+      name: ability.name.capitalize,
+      effect: effect
+    )
 
-#     if a.persisted?
-#       PokemonAbility.create(pokemon: pokemon, ability: a)
-#     end
-#   end
+    if a.persisted?
+      PokemonAbility.create(pokemon: pokemon, ability: a)
+    end
+  end
 
-#   specie = PokeApi.get(pokemon_species: pokemon.name)
+  specie = PokeApi.get(pokemon_species: pokemon.name)
 
-#   flavor_text = ""
-#   genus = ""
+  flavor_text = ""
+  genus = ""
 
-#   specie.flavor_text_entries.each do |entry|
-#     flavor_text = entry.flavor_text.squish unless entry.language.name != "en" || entry.version.name != "red"
-#   end
+  specie.flavor_text_entries.each do |entry|
+    flavor_text = entry.flavor_text.squish unless entry.language.name != "en" || entry.version.name != "red"
+  end
 
-#   specie.genera.each do |gene|
-#     genus = gene.genus.squish unless gene.language.name != "en"
-#   end
+  specie.genera.each do |gene|
+    genus = gene.genus.squish unless gene.language.name != "en"
+  end
 
-#   new_specie = pokemon.create_species(
-#     name: specie.name.capitalize,
-#     flavor_text: flavor_text,
-#     genus: genus
-#   )
-#   new_specie.save
+  new_specie = pokemon.create_species(
+    name: specie.name.capitalize,
+    flavor_text: flavor_text,
+    genus: genus
+  )
+  new_specie.save
 
-#   puts "Created #{pokemon.name.capitalize} entry"
-# end
+  puts "Created #{pokemon.name.capitalize} entry"
+end
