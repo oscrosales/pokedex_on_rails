52d1bf8563fb4e101b56191cbe3b0aa17048c379
oscrosales <orosales2@academic.rrc.ca>
2025-03-06 16:50:21 -0600

    Added Abilities controller

---
+++ app/controllers/abilities_controller.rb
@@ -0,0 +1,7 @@
+class AbilitiesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get "types/index"
-  get "types/show"
   root to: "pokemons#index"
   resources :pokemons, only: [ :index, :show ]
   resources :types, only: [ :index, :show ]
+  resources :abilities, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
