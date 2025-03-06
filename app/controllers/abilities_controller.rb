438b6d1ccf63a26657630005698cf78a672f49c8
oscrosales <orosales2@academic.rrc.ca>
2025-03-06 16:51:49 -0600

    Updated Abilities controller

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -1,7 +1,11 @@
 class AbilitiesController < ApplicationController
   def index
+    @abilities = Ability.all.order("name ASC")
   end
 
   def show
+    @ability = Ability.includes(:pokemons)
+                      .find_by('name = ? OR id = ?', params[:id], params[:id])
+    @pokemons = @ability.pokemons
   end
 end
