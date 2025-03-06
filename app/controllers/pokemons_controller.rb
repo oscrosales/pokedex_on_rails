d38f25cb3330bda09a6066a72cd1dd2ad6d12192
oscrosales <orosales2@academic.rrc.ca>
2025-03-05 18:26:55 -0600

    Fixed n+1 problem and did stuff to show page

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,11 @@
 class PokemonsController < ApplicationController
   def index
+    @pokemons = Pokemon.all
   end
 
   def show
+    @pokemon = Pokemon.includes(:types, :abilities)
+                      .find(params[:id])
+
   end
 end
